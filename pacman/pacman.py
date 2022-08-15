a9b903576edda9f66b9071527169164bd705f6ab
VitorOM2 <viktormarque2053@gmail.com>
2022-08-14 21:32:44 -0300

    Adicionado movimento ao circulo

--- pacman/pacman.py
+++ pacman/pacman.py
@@ -3,13 +3,37 @@ import pygame
 pygame.init()
 
 AMARELO = (255, 255, 0)
+PRETO = (0, 0, 0)
+RAIO = 30
+VELOCIDADE = 0.1
 
 tela = pygame.display.set_mode((640, 480), 0)
+x = 320
+y = 240
+vel_x = VELOCIDADE
+vel_y = VELOCIDADE
 
 while True:
 
+    # Regras
+
+    # ===== Movimento =====
+    x += vel_x
+    y += vel_y
+
+    if x + RAIO > 640:
+        vel_x = -VELOCIDADE
+    if x - RAIO < 0:
+        vel_x = VELOCIDADE
+
+    if y + RAIO > 480:
+        vel_y = -VELOCIDADE
+    if y - RAIO < 0:
+        vel_y = VELOCIDADE
+
     # Pinta
-    pygame.draw.circle(tela, AMARELO, (320, 240), 50, 0)
+    tela.fill(PRETO)
+    pygame.draw.circle(tela, AMARELO, (x, y), 30, 0)
     pygame.display.update()
 
     # Eventos
